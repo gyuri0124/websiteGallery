fa7e6610dbac1546db5c9f1ed9c4e525f9bb5fce
gyuri0124 <gyul12243@naver.com>
2025-02-18 15:11:30 +0900

    홈페이지 사진 배치 코드 수정 필요(반응형)

---
+++ js/gallery.js
@@ -0,0 +1,24 @@
+/* 배경화면 */
+const bg = document.querySelector(".background");
+bg.style.opacity = "40%";
+
+/* 원본 띄우기 */
+document.addEventListener("DOMContentLoaded", function() {
+  const image = document.querySelectorAll(".pic");
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modalImg");
+  const closeBtn = document.querySelector(".close");
+
+  // 이미지 클릭 시 모달 열기
+  image.forEach(img => {
+    img.addEventListener("click", function() {
+      modal.style.display = "flex";
+      modalImg.src = this.src;
+    });
+  });
+
+  // 닫기 버튼 클릭 시 모달 닫기
+  closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+  });
+});
