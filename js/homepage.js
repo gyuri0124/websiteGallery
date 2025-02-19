8caa8a0c1fa67c654ae1c71e79471d90ad32c119
gyuri0124 <gyul12243@naver.com>
2025-02-19 13:26:13 +0900

    메뉴 링크 연결

---
+++ js/homepage.js
@@ -0,0 +1,13 @@
+// 메뉴 버튼 눌렀을 때
+function open1(){
+  window.open("https://gyuri0124.github.io/websiteGallery/", "_self");
+}
+function open2(){
+  window.open("https://gyuri0124.tistory.com", "_blank");
+}
+function open3(){
+  window.open("https://kr.pinterest.com/ideas/", "_blank");
+}
+function open4(){
+  window.open("https://namu.wiki/w/Collection", "_blank");
+}
